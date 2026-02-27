ca4bccbdac3cec31adc028ec6ee74919f4ae9b05
gs-hkillari <hkillari@gainsight.com>
2026-02-27 21:36:13 +0530

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/krishna-temple/',
   plugins: [react()],
   server: {
     port: 4201
